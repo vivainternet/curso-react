1abd0d1a409c6b05d369856bb6a620512c9cd2f6
vivainternet <vivainternet@gmail.com>
2024-10-27 16:12:27 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Card from "./components/Card";
+import Table from "./components/Table";
+
+
+function App() {
+    /* return <Card /> */
+    return <Table />
+}
+
+export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,47 @@
+import { Fragment } from "react";
+
+function Table() {
+
+	return (
+		<table className="table table-bordered border-primary">
+			<thead className="table-light table-bordered border-primary">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">First</th>
+					<th scope="col">Last</th>
+					<th scope="col">Handle</th>
+				</tr>
+			</thead>
+			<TableBody />
+		</table>
+	);
+}
+
+/* <> es <Fragment> */
+export function TableBody() {
+	return (
+		<Fragment>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope="row">2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope="row">3</th>
+					<td>Larry</td>
+					<td>the Bird</td>
+					<td>@twitter</td>
+				</tr>
+			</tbody>
+		</Fragment>
+	);
+}
+export default Table;
