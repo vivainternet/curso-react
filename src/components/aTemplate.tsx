f8e23e2115007bf7c3217e2d4ca01d35d477e9e2
vivainternet <vivainternet@gmail.com>
2024-10-28 08:45:58 -0300

    Template

---
+++ src/components/aTemplate.tsx
@@ -0,0 +1,13 @@
+// Se crea con Control + Alt + R: tsrafce (antes instalar el snipet ES7+React/Redux/React-Native de dsznajder)
+
+import React from 'react'
+
+type Props = {}
+
+function Form({ }: Props) {
+    return (
+        <div>Form</div>
+    )
+}
+
+export default Form
