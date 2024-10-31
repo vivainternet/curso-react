f010d3348229463602f1e2b0fe3a85ceaec20692
vivainternet <vivainternet@gmail.com>
2024-10-31 00:19:54 -0300

    Template: tsrfce

--- src/components/aTemplate.tsx
+++ src/components/aTemplate.tsx
@@ -1,5 +1,5 @@
 /*
- * Templatede Componentes: Se crea con Control + Alt + R: tsrafce 
+ * Templatede Componentes: Se crea con Control + Alt + R: tsrfce 
  * (antes instalar el snipet ES7+React/Redux/React-Native de dsznajder)
 **/
 import React from 'react'
