047f26742377790a4fc51eff5fa6d910753dd1c5
vivainternet <vivainternet@gmail.com>
2024-11-07 20:58:38 -0300

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css' //import './index.css' reemplazado por bootstrap.
+/* import 'bootstrap/dist/css/bootstrap.min.css'  */ // Descomentar para activar Bootstrap
+import './index.css' // reemplazado por bootstrap.
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
